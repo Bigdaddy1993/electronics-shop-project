6145c65207a33fa09a6e976406367ba7982fb75f
Roman Gerasimov <rgerasimov58@gmail.com>
2023-11-25 18:53:15 +0300

    Homework done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,31 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+
+def test_apply_discount(item):
+    if item.price == 0.8:
+        assert item.price == 8000.0
+
+
+@pytest.fixture
+def item_notebook():
+    return Item("Ноутбук", 20000, 5)
+
+
+def test_calculate_total_price_notebook(item_notebook):
+    assert item_notebook.calculate_total_price() == 100000
+
+
+def test_apply_discount_notebook(item_notebook):
+    if item_notebook.price == 0.8:
+        assert item_notebook.price == 20000
