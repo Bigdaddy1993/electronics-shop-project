950f029b126302929c1b48491c9780192e33e3dd
Roman Gerasimov <rgerasimov58@gmail.com>
2023-12-08 07:16:45 +0300

    add docstrings

--- src/item.py
+++ src/item.py
@@ -22,9 +22,19 @@ class Item:
         self.quantity = quantity
 
     def __repr__(self):
+        """
+
+        :return: Магический метод для отображения информации об объекте класса
+        (для разработчиков)
+        """
         return f"{self.__class__.__name__}('{self.name}', '{self.price}, '{self.quantity}')"
 
     def __str__(self):
+        """
+
+        :return: Магический метод для отображения информации об объекте класса
+        (для пользователей)
+        """
         return self.name
 
     @property
